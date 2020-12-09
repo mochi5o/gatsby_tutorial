7f291fd8fca16bd39206b9348c548310b66cffb5
mochi5o <1610.baumkuchen.124@gmail.com>
2020-12-09 23:28:42 +0900

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Header headerText="About main" />
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
 }
